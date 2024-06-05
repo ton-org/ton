e6da10503d74224a85a67a61e543c778ca7f69c3
siandreev <andreev.sergey.i@yandex.ru>
2024-06-05 19:03:51 +0200

    fix: replace Wallet3 with WalletV3

--- src/wallets/WalletContractV3.ts
+++ src/wallets/WalletContractV3.ts
@@ -3,12 +3,12 @@ import { Maybe } from "../utils/maybe";
 import { SingedAuthSendArgs, ExternallySingedAuthSendArgs } from "./signing/singer";
 
 
-export type Wallet3BasicSendArgs = {
+export type WalletV3BasicSendArgs = {
     seqno: number,
     messages: MessageRelaxed[]
     sendMode?: Maybe<SendMode>,
     timeout?: Maybe<number>,
 }
 
-export type SingedAuthWallet3SendArgs = Wallet3BasicSendArgs & SingedAuthSendArgs;
-export type ExternallySingedAuthWallet3SendArgs = Wallet3BasicSendArgs & ExternallySingedAuthSendArgs;
+export type SingedAuthWallet3SendArgs = WalletV3BasicSendArgs & SingedAuthSendArgs;
+export type ExternallySingedAuthWallet3SendArgs = WalletV3BasicSendArgs & ExternallySingedAuthSendArgs;
