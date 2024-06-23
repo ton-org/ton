994f1928ec15468c74eda89ad85fa66046584f04
Nikolai Dorofeev <dorich2000@gmail.com>
2024-06-23 08:51:53 +0400

    Regroup types

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,11 +1,12 @@
 import { TupleItem, TupleReader } from '@ton/core';
+import type { StackItem } from './types';
 import { parseStackItem } from './parseStackItem';
 
-export function parseStack(src: any[]) {
+export function parseStack(src: unknown[]) {
   let stack: TupleItem[] = [];
 
   for (let s of src) {
-      stack.push(parseStackItem(s));
+      stack.push(parseStackItem(s as StackItem));
   }
 
   return new TupleReader(stack);
