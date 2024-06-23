b87c3a99684ed4b5efca2a43bfbd1cb222b0e6e2
Nikolai Dorofeev <dorich2000@gmail.com>
2024-06-23 08:54:49 +0400

    Format parseObject

---
+++ src/parser/parseTvmValue.ts
@@ -0,0 +1,27 @@
+import { Cell, type TupleItem } from '@ton/core'
+import type { TvmValue } from './types'
+
+export function parseTvmValue(x: TvmValue): TupleItem {
+  const typeName = x['@type']
+  switch (typeName) {
+    case 'tvm.list':
+    case 'tvm.tuple':
+      return { type: 'tuple', items: x.elements.map(parseTvmValue) }
+    case 'tvm.cell':
+      return { type: 'cell', cell: Cell.fromBoc(Buffer.from(x.bytes, 'base64'))[0] }
+    case 'tvm.stackEntryCell':
+      return parseTvmValue(x.cell)
+    case 'tvm.stackEntryTuple':
+      return parseTvmValue(x.tuple)
+    case 'tvm.stackEntryNumber':
+      return parseTvmValue(x.number)
+    case 'tvm.numberDecimal':
+      return { type: 'int', value: BigInt(x.number) }
+    case 'tvm.slice':
+      return { type: 'slice', cell: Cell.fromBoc(Buffer.from(x.bytes, 'base64'))[0] }
+    case 'tvm.stackEntrySlice':
+      return parseTvmValue(x.slice)
+    default:
+      throw Error('Unsupported item type: ' + typeName)
+  }
+}
