889bd50831481c0f0efa0201177308fd6ff68c29
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2024-12-01 13:04:33 +0400

    Move types to a single util file

--- src/utils/stack.ts
+++ src/utils/stack.ts
@@ -1,5 +1,29 @@
 import { z } from 'zod';
-import type { TvmTuple, TvmList } from './tvmValues';
+
+// Plain types for TON Virtual Machine values
+type TvmNumberDecimal = { '@type': 'tvm.numberDecimal', number: string };
+type TvmStackEntryNumber = { '@type': 'tvm.stackEntryNumber', number: TvmNumberDecimal };
+
+type TvmSlice = { '@type': 'tvm.slice', bytes: string };
+type TvmStackEntrySlice = { '@type': 'tvm.stackEntrySlice', slice: TvmSlice };
+
+type TvmCell = { '@type': 'tvm.cell', bytes: string };
+type TvmStackEntryCell = { '@type': 'tvm.stackEntryCell', cell: TvmCell };
+
+// Structured types for TON Virtual Machine values
+export type TvmList = { '@type': 'tvm.list', elements: TvmValue[] };
+type TvmStackEntryList = { '@type': 'tvm.stackEntryList', list: TvmList };
+
+export type TvmTuple = { '@type': 'tvm.tuple', elements: TvmValue[] };
+type TvmStackEntryTuple = { '@type': 'tvm.stackEntryTuple', tuple: TvmTuple };
+
+// Union of all TON Virtual Machine values
+type TvmCommonValue = TvmNumberDecimal | TvmCell | TvmSlice | TvmList | TvmTuple;
+type TvmStackEntryValue = TvmStackEntryCell | TvmStackEntryNumber | TvmStackEntrySlice | TvmStackEntryList | TvmStackEntryTuple;
+export type TvmValue = TvmCommonValue | TvmStackEntryValue;
+
+
+// zod definitions
 
 const serializedCellSchema = z.object({
   bytes: z.string(),
